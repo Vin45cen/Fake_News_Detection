5384f8d3ef8972f2f8c570f4e13f9689d26a7cb6
Vin45cen <vincenttanjaya588@gmail.com>
2024-10-15 14:13:14 +0700

    commit3

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ def main():
     st.title("Fake News Detector")
     text = st.text_input("Input Text Here: ")
     text_button = st.button("Submit")
+    clear_button = st.button("Clear Text")
 
 if __name__ == '__main__':
     main()
