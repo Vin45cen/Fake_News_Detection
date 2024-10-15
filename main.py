77d89272fdf111c26e43fe4efaa96a128d5271f2
Vin45cen <vincenttanjaya588@gmail.com>
2024-10-15 14:32:19 +0700

    commit ke 4

--- main.py
+++ main.py
@@ -2,9 +2,21 @@ import streamlit as st
 
 def main():
     st.title("Fake News Detector")
-    text = st.text_input("Input Text Here: ")
+    input_text = st.text_input("Input Text Here: ")
     text_button = st.button("Submit")
     clear_button = st.button("Clear Text")
+
+    if st.button("Submit"):
+        if input_text:
+            pass
+            # output = classify_text(input_text)
+            # st.write(f"Prediction: {output}")
+        else:
+            st.write("Please enter text")
+
+    if st.button("Clear Text"):
+        # input_text = ""  # This will clear the input_text variable
+        st.experimental_rerun()
 
 if __name__ == '__main__':
     main()
