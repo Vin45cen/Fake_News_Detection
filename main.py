a5dedcb819dea6e31681d4ff0721d4a2cc779cd0
Vin45cen <vincenttanjaya588@gmail.com>
2024-10-18 13:14:46 +0700

    remove temp.py

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 import streamlit as st
+import pickle
 
 def main():
+    with open('nlp_pipeline.pkl', 'rb') as file:
+        model_pipeline = pickle.load(file)
+
     st.title("Fake News Detector")
-    input_text = st.text_input("Input Text Here: ")
-    text_button = st.button("Submit")
-    clear_button = st.button("Clear Text")
+    input_text = st.text_area("Input Text Here: ")
+    # text_button = st.button("Submit")
+    # clear_button = st.button("Clear Text")
 
     if st.button("Submit"):
-        if input_text:
-            pass
-            # output = classify_text(input_text)
-            # st.write(f"Prediction: {output}")
-        else:
-            st.write("Please enter text")
-
-    if st.button("Clear Text"):
-        # input_text = ""  # This will clear the input_text variable
-        st.experimental_rerun()
+        prediction = model_pipeline.predict([input_text])[0]
+        print(prediction)
+        if prediction == 'True':
+            st.write("Hasil Prediksi : Positive")
+        elif prediction == 'Fake':
+            st.write("Hasil Prediksi : Negative")
+        
 
 if __name__ == '__main__':
     main()
